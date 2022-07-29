8766c2683021e902765c8d9eeacad4973b6485db
Mehran Rezaei <mehranrezaei.dev@gmail.com>
2022-07-29 17:52:08 +0430

    first and final commmit

---
+++ script.js
@@ -0,0 +1,46 @@
+// `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric 
+
+const form = document.querySelector(".top-banner form")
+const input = document.querySelector(".top-banner input")
+const msg = document.querySelector(".top-banner .msg")
+const list = document.querySelector(".ajax-section .cities")
+
+ const apiKey = "2d928a6459da52ad23361e4ed52ffa0b"
+ form.addEventListener("submit", e => {
+     e.preventDefault();
+    let inputVal = input.value 
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric `
+    fetch(url)
+    .then((Response) => Response.json())
+    .then((data) => {
+        console.log(data)
+        const {main , name, sys , weather} = data;
+        const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@4x.png`
+        // const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`
+       console.log(icon)
+       const li = document.createElement("li")
+       li.classList.add("city")
+       const markup = `
+       <h2 class='city-name' data-name=${name},${sys.country}>
+         <span>${name}</span>
+         <span>${sys.country}</span>
+       </h2> 
+       <div class='city-temp'>${Math.round(main.temp)}</div>
+       <figure>
+           <img class='city-icon' src='${icon}' alt = 'city'>
+           <figurecaption>${weather[0]["description"]}</figurecaption>
+       </figure>
+       `;
+       li.innerHTML = markup;
+       list.appendChild(li)
+       msg.innerText = ""
+    })
+    .catch(()=> {
+        msg.innerText = "Search for a valid city"
+    })
+        input.value = ""
+ })
+
+
+
